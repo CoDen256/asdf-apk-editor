6d5fef875e7a8ebc564bbfe455de47456380067f
Denys Chernyshov <den.blackshov@gmail.com>
2025-01-22 14:36:19 +0100

    test for youtube dl

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,71 +2,67 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for apk-editor.
-GH_REPO="https://github.com/REAndroid/APKEditor"
-TOOL_NAME="apk-editor"
-TOOL_TEST="apk-editor --help"
+GH_REPO="https://github.com/ytdl-org/youtube-dl"
+GH_RELEASES="https://api.github.com/repos/ytdl-org/youtube-dl/releases"
+TOOL_NAME="youtube-dl"
+TOOL_TEST="youtube-dl --help"
 
 fail() {
-	echo -e "asdf-$TOOL_NAME: $*"
-	exit 1
+  echo -e "asdf-${TOOL_NAME}: $*" >&2
+  exit 1
 }
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if apk-editor is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
+  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
 
 sort_versions() {
-	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
-}
-
-list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^V//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
 list_all_versions() {
-	list_github_tags
+  curl "${curl_opts[@]}" --url "$GH_RELEASES" |
+    grep -oE "tag_name\": *\".{1,15}\"," | sed -e 's/tag_name\": \"v*//' -e 's/\",//' || fail "Could not fetch versions"
 }
 
 download_release() {
-	local version filename url
-	version="$1"
-	filename="$2"
+  local version
+  local filename
+  version="$1"
+  filename="$2"
 
-	# TODO: Adapt the release URL convention for apk-editor
-	url="$GH_REPO/releases/download/V${version}/APKEditor-${version}.jar"
+  if [ "$(uname -s)" != "Linux" ]; then
+    fail "This plugin only supports installing youtube-dl on Linux"
+  fi
 
-	echo "* Downloading $TOOL_NAME release $version..."
-	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+  local url
+  url="${GH_REPO}/releases/download/${version}/youtube-dl"
+  echo "* Downloading ${TOOL_NAME} release ${version}..."
+  curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download ${url}"
 }
 
 install_version() {
-	local install_type="$1"
-	local version="$2"
-	local install_path="${3%/bin}/bin"
+  local install_type
+  local version
+  local install_path
 
-	if [ "$install_type" != "version" ]; then
-		fail "asdf-$TOOL_NAME supports release installs only"
-	fi
+  install_type="$1"
+  version="$2"
+  install_path="${3%/bin}/bin"
 
-	(
-		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+  if [ "$install_type" != "version" ]; then
+    fail "asdf-${TOOL_NAME} supports release installs only"
+  fi
 
-		# TODO: Assert apk-editor executable exists.
-		local tool_cmd
-		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
-
-		echo "$TOOL_NAME $version installation was successful!"
-	) || (
-		rm -rf "$install_path"
-		fail "An error occurred while installing $TOOL_NAME $version."
-	)
+  (
+    mkdir -p "$install_path"
+    install -m 755 "${ASDF_DOWNLOAD_PATH}/youtube-dl" "$install_path" || fail "Installation failed"
+    echo "$TOOL_NAME $version installation was successful!"
+  ) || (
+    rm -rf "$install_path"
+    fail "An error occurred while installing $TOOL_NAME $version."
+  )
 }
